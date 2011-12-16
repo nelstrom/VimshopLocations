3afe52678897d8de434f2f11ce9b2bba3cc5f13b
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-16 18:09:39 +0100

    Can import subscribers.

--- app/models/subscriber.rb
+++ app/models/subscriber.rb
@@ -1,9 +1,45 @@
 class Subscriber < ActiveRecord::Base
+
+  validates_presence_of :email
+  validates_uniqueness_of :email
+
+  def self.fetch_grouped_emails(limit=50)
+    emails = fetch_emails
+    subs = []
+    while emails.present?
+      subs.push emails.slice!(0,limit)
+    end
+    subs
+  end
+
   def self.fetch_emails
     h = Hominid::API.new(MAILCHIMP_API_KEY)
     members = h.listMembers(WORKSHOP_LIST_ID, 'subscribed', 100.years.ago, 0, 15000)
     if members.present? && data = members['data']
-      data.map { |v| v['email'] } 
+      data.map { |v| v['email'] }
     end
+  end
+
+  def self.import_subscribers
+    fetch_grouped_emails.each do |group|
+      info = fetch_info_for(group)
+      info['data'].each do |u|
+        fields = u['merges']
+        unless Subscriber.find_by_email(u['email']).present?
+          Subscriber.create({
+            :email => u['email'],
+            :city       => fields['CITY'],
+            :country    => fields['COUNTRY'],
+            :first_name => fields['FNAME'],
+            :last_name  => fields['LNAME']
+          })
+        end
+      end
+    end
+  end
+
+  def self.fetch_info_for(emails)
+    h = Hominid::API.new(MAILCHIMP_API_KEY)
+    h.listMemberInfo(WORKSHOP_LIST_ID, emails)
   end
 end
--- db/migrate/20111216142320_create_subscribers.rb
+++ db/migrate/20111216142320_create_subscribers.rb
@@ -1,8 +1,15 @@
 class CreateSubscribers < ActiveRecord::Migration
   def change
     create_table :subscribers do |t|
+      t.string :email, :null => false
+      t.string :city
+      t.string :country
+      t.string :first_name
+      t.string :last_name
 
       t.timestamps
     end
+
+    add_index :subscribers, :email
   end
 end
