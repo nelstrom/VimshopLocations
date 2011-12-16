b96f10a579731453826528e90fcb36a993adc1cd
Drew Neil <andrew.jr.neil@gmail.com>
2011-12-16 15:54:58 +0100

    Initial commit with some code.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+VimshopLocations::Application.config.secret_token = 'dc5dd0149b7922e848996648b936099688b24034fbd13cfe24706675d2caf82d20b52aac738644b0ddd094d233680b42efd9e201b96effafcbcbeeee0973a151'
